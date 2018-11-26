e2842037f2209405abe676ac1937bf4bfbda3fb2
javajb99 <45365212+javajb99@users.noreply.github.com>
2018-11-26 19:54:51 +0200

    Add files via upload

--- BankAspect.java
+++ BankAspect.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -10,6 +12,18 @@ public class BankAspect {
 	public void beforeGetMoney()
 	{
 		System.out.println("I hope I have enough money!!");
+	}
+	
+	@After("execution(public int get*(int))")
+	public void afterGetMoney()
+	{
+		System.out.println("I got the money!! yeah!!");
+	}
+	
+	@AfterThrowing(pointcut = "within(com.example.demo.Bank)", throwing="ex")
+	public void afterThownException(Exception ex)
+	{
+		System.out.println("Watch out! exception is about to be thrown!! " + ex.getMessage());
 	}
 	
 }
